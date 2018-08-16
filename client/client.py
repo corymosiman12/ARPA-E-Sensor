f50037ef22ffb21cd928ae423ebf62c0e5a3c8b6
Cory Mosiman <corymosiman@engr2gw-0-134-dhcp.int.colorado.edu>
2018-08-16 16:41:34 -0600

    Upate my_photo and client script to succesfully capture and write photo at 1s interval

--- client/client.py
+++ client/client.py
@@ -2,15 +2,26 @@ import my_photo
 import json
 import socket
 import sys
+import os
 from datetime import datetime
+import threading
+import logging
+import time
+
+logging.basicConfig(filename = 'client_logfile.log', level = logging.DEBUG,
+                    format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%d-%m-%Y:%H:%M:%S',)
 
 def import_conf(config_file, server_id):
     with open(config_file, 'r') as f:
-        ip_line = [line.strip('\n') for line in f if server_id in line]
-        assert len(ip_line) == 1
-        return ip_line[0].split(" ")[1]
+        conf = [line.strip('\n') for line in f if server_id in line 
+                or ('root' in line and not '#' in line) 
+                or ("stream" in line and not '#' in line)]
+        # print(conf)
+        assert len(conf) == 3
+        return((conf[0].split(" ")[1], conf[1].split(" ")[1], conf[2].split(" ")[1]))
 
-def create_message(params = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "temp", "rh"]):
+def create_message(params):
     """
     Join all parameters with a carriage return and newline and
     send to server. The default parameters are:
@@ -19,7 +30,11 @@ def create_message(params = [datetime.now().strftime("%Y-%m-%d %H:%M:%S"), "temp
         3. rh
         4. ...ELSE...
     """
-    # dt_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Always include the datetime as the first line item in
+    # a message sent to the server
+    dt_str = [datetime.now().strftime("%Y-%m-%d %H:%M:%S")]
+    for p in params:
+        dt_str.append(p)
     params = '\r\n'.join(params)
     print(params)
 
@@ -27,7 +42,7 @@ def get_data(server_ip):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    test_file = "/Users/corymosiman/Dropbox/ARPA-E SENSOR Project Folder/3 Tasks/1 HPDmobile/Documents/secondPurchaseDiagram.pdf"
+    # test_file = "/Users/corymosiman/Dropbox/ARPA-E SENSOR Project Folder/3 Tasks/1 HPDmobile/Documents/secondPurchaseDiagram.pdf"
     with open(test_file, 'rb') as f:
         a = f.read()
     print(a)
@@ -40,8 +55,30 @@ def get_data(server_ip):
     s.close()
 
 if __name__ == "__main__":
+    """
+    The client must be run by specifying a server id. Example:
+        client$ python client.py S1
+    """
     server_id = sys.argv[1]
-    server_ip = import_conf('client.conf', server_id)
-    print(server_ip)
-    create_message()
-    # get_data(server_ip)
+
+    # Obtain server ip address and external drive root directory for saving data
+    pi_ip_address, ext_root, stream_type = import_conf('client.conf', server_id)
+
+    # Define directory for saving photos
+    img_dir = os.path.join(ext_root, server_id, "data", "img")
+    # print(img_dir)
+    # print(stream_type)
+    
+    # vid_even = my_photo.MyPhoto(img_dir, pi_ip_address, stream_type, 0)
+    vid_even = my_photo.MyPhoto2(img_dir, pi_ip_address, stream_type, 0)
+    vid_odd = my_photo.MyPhoto2(img_dir, pi_ip_address, stream_type, 1)
+
+
+# sudo service uv4l_raspicam restart
+# sudo nano /etc/uv4l/uv4l-raspicam.conf
+# interface eth0
+# static ip_address=192.168.0.3
+# static routers=192.168.0.1
+# static domain_name_servers=192.168.0.1
+
+
--- client/my_photo.py
+++ client/my_photo.py
@@ -1,39 +1,176 @@
-from cv2 import *
+import cv2
 from datetime import datetime
 import os
 import pickle
 import logging
+import time
+import threading
+from imutils.video import WebcamVideoStream
+import imutils
 
 # TODO:
 # 1. Add error checking and logging to script
 # 2. Make sure script works when connected over local wLAN through linksys router
 
-
 # Initialize camera.  The pi uv4l server must be running at the defined IP address
 # and port number specified below.  The path: /stream/video.mjpeg can be found in the 
 # pi's 192.168.0.4:8080/info
 
-def capture_photo_from_pi(base_path, pi_ip_address, pi_id):
-    path = os.path.join(base_path, pi_id)
-    stream_path = "http://" + pi_ip_address + ":8080/stream/video.mjpeg"
-    cam = VideoCapture(stream_path)
+# logging.basicConfig(filename = 'client_logfile.log', level = logging.DEBUG,
+#                     format='%(asctime)s,%(msecs)d %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+#                     datefmt='%d-%m-%Y:%H:%M:%S',)
 
-    # Run infinitely
-    while 1:
-        s, img = cam.read()
-        fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
-        
-        # if frame captured without errors
-        if s:
-            try:
-                # Convert image to greyscale
-                img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
 
-                # Write to disk
-                imwrite(os.path.join(path,fname), img)
-            except Exception as e:
-                logging.CRITICAL("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+class MyPhoto(threading.Thread):
+    def __init__(self, img_dir, pi_ip_address, stream_type, sec):
+        threading.Thread.__init__(self)
+        self.img_dir = img_dir
+        self.pi_ip_address = pi_ip_address
+        self.stream_type = stream_type
+        self.sec = sec
+        self.video_status = False
+        self.connect_to_video()
+        self.start()
+            
+    def connect_to_video(self):
+        if self.stream_type == "mjpeg":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.mjpeg"
+        elif self.stream_type == "h264":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.h264"
+        elif self.stream_type == "jpeg":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.jpeg"
+        self.cam = cv2.VideoCapture(stream_path)
+        while not self.cam.isOpened():
+            print("Unsuccessful connection for self.sec = {}".format(self.sec))
+            self.cam = cv2.VideoCapture(stream_path)
+            self.video_status = False
+            time.sleep(1)
+        self.video_status = True
+        print("Connected for self.sec = {}".format(self.sec))
+
+    def run(self):
+        while 1:
+            if datetime.now().second % 2 == self.sec:
+                # print("Capturing image for second == {}".format(datetime.now().second))
+                # logging.DEBUG("Capturing image for second == {}".format(sec))
+                fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
+                capture_status, img = self.cam.read()
+                # fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
+                
+                # if frame captured without errors
+                if capture_status:
+                    try:
+                        # Convert image to greyscale
+                        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+                        # Write to disk
+                        cv2.imwrite(os.path.join(self.img_dir,fname), img)
+                    except Exception as e:
+                        print("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+                        # logging.CRITICAL("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+                
+                elif not capture_status:
+                    print("Capture from pi failed for second: {}".format(self.sec))
+                    print("Reconnecting to server")
+                    self.video_status = False
+                    self.connect_to_video()
+                    # logging.CRITICAL("Capture from pi failed for file: {}".format(fname))
+                time.sleep(0.99999)
+
+class MyPhoto2(threading.Thread):
+    def __init__(self, img_dir, pi_ip_address, stream_type, sec):
+        threading.Thread.__init__(self)
+        self.img_dir = img_dir
+        self.pi_ip_address = pi_ip_address
+        self.stream_type = stream_type
+        self.sec = sec
+        self.video_status = False
+        self.connect_to_video()
+        self.start()
+            
+    def connect_to_video(self):
+        if self.stream_type == "mjpeg":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.mjpeg"
+        elif self.stream_type == "h264":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.h264"
+        elif self.stream_type == "jpeg":
+            stream_path = "http://" + self.pi_ip_address + ":8080/stream/video.jpeg"
         
-        elif not s:
-            logging.CRITICAL("Capture from pi failed for file: {}".format(fname))
-    
+        self.cam = WebcamVideoStream(stream_path).start()
+        while not self.cam.stream.isOpened():
+            print("Unsuccessful connection for self.sec = {}".format(self.sec))
+            self.cam = WebcamVideoStream(stream_path).start()
+            self.video_status = False
+            time.sleep(1)
+        self.video_status = True
+        print("Connected for self.sec = {}".format(self.sec))
+
+    def run(self):
+        while 1:
+            if datetime.now().second % 2 == self.sec:
+                # print("Capturing image for second == {}".format(datetime.now().second))
+                # logging.DEBUG("Capturing image for second == {}".format(sec))
+                fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
+                img = self.cam.read()
+                # fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
+                
+                try:
+                    # Convert image to greyscale
+                    img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+                    # Write to disk
+                    cv2.imwrite(os.path.join(self.img_dir,fname), img)
+                except Exception as e:
+                    print("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+                    # logging.CRITICAL("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+            
+            # elif not capture_status:
+            #     print("Capture from pi failed for second: {}".format(self.sec))
+            #     print("Reconnecting to server")
+            #     self.video_status = False
+            #     self.connect_to_video()
+            #     # logging.CRITICAL("Capture from pi failed for file: {}".format(fname))
+            # time.sleep(0.99999)
+
+#####################################################################################
+##################################### LEFTOVERS #####################################
+    # def valid_second(self):
+    #     latest = self.sec_list[-1] + 2
+    #     while latest < 60:
+    #         self.sec_list.append(latest)
+    #         latest += 2
+    #     print(self.sec_list)
+
+# def capture_photo_from_pi(img_dir, pi_ip_address, sec):
+#     print("Thread started for video capture second == {}".format(sec))
+#     # logging.DEBUG("Thread started for video capture second == {}".format(sec))
+#     stream_path = "http://" + pi_ip_address + ":8080/stream/video.mjpeg"
+#     try:
+#         cam = VideoCapture(stream_path)
+#     except Exception:
+#         exit()
+
+#     # Run infinitely
+#     while 1:
+#         if datetime.now().second == sec:
+#             print("Capturing image for second == {}".format(sec))
+#             # logging.DEBUG("Capturing image for second == {}".format(sec))
+#             s, img = cam.read()
+#             fname = datetime.now().strftime("%Y-%m-%d %H%M%S_grey.png")
+            
+#             # if frame captured without errors
+#             if s:
+#                 try:
+#                     # Convert image to greyscale
+#                     img = cv2.cvtColor( img, cv2.COLOR_RGB2GRAY)
+
+#                     # Write to disk
+#                     imwrite(os.path.join(img_dir,fname), img)
+#                 except Exception as e:
+#                     print("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+#                     # logging.CRITICAL("Unable to convert to grayscale and write to disk.  Error: {}.  File: {}".format(e, fname))
+            
+#             elif not s:
+#                 print("Capture from pi failed for file: {}".format(fname))
+#                 # logging.CRITICAL("Capture from pi failed for file: {}".format(fname))
+#             time.sleep(1)
