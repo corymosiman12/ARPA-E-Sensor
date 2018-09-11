5e685bf300184385b788845f73968c8150a3bca2
Cory Mosiman <cory.mosiman12@gmail.com>
2018-09-11 07:55:53 -0600

    Modify tests

--- server/testSGP30.py
+++ server/testSGP30.py
@@ -2,16 +2,18 @@ import hpd_sensors
 import sys
 from datetime import datetime
 import os
+import time
 
 def main(id):
     size = 1
     test = hpd_sensors.HPD_SGP30()
     d = {}
     while size <= 60:
-        if datetime.now().microsecond == 0:
-            d[size] = test.read()
-            size += 1
-    
+        d[size] = test.read()
+        print("Second: {}\tCurrent value: {}".format(size, d[size]))
+        size += 1
+        time.sleep(1)
+
     fname = os.path.join('tests','SGP30_{}.txt'.format(id))
     with open(fname, 'w+') as f:
         for k, v in d.items():
@@ -20,5 +22,5 @@ def main(id):
 
 if __name__=="__main__":
     id = sys.argv[1]
-    print("Sampling SGP30 id: {}")
+    print("Sampling SGP30 id: {}".format(id))
     main(id)
--- server/testVL53L1X.py
+++ server/testVL53L1X.py
@@ -2,15 +2,17 @@ import hpd_sensors
 import sys
 from datetime import datetime
 import os
+import time
 
 def main(id):
     size = 1
     test = hpd_sensors.HPD_VL53L1X()
     d = {}
     while size <= 60:
-        if datetime.now().microsecond == 0:
-            d[size] = test.read()
-            size += 1
+        d[size] = test.read()
+        print("Second: {}\tCurrent value: {}".format(size, d[size]))
+        size += 1
+        time.sleep(1)
     
     fname = os.path.join('tests','VL53L1X_{}.txt'.format(id))
     with open(fname, 'w+') as f:
@@ -20,5 +22,5 @@ def main(id):
 
 if __name__=="__main__":
     id = sys.argv[1]
-    print("Sampling VL53L1X id: {}")
+    print("Sampling VL53L1X id: {}".format(id))
     main(id)
