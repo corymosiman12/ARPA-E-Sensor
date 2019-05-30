fbd207c34a2393e757111946cea3932e769f2e6e
mhsjacoby <maja8167@colorado.edu>
2019-05-30 12:04:31 -0600

    Reinstate old code to recover photo_checker function
    Revert "remove duplicate my_recv_all from MyPhoto"
    
    This reverts commit 2a7b04888ba261d588aec1d9f0c95e3fbb8ccda8.

--- client/client.py
+++ client/client.py
@@ -347,51 +347,51 @@ class MyPhoto(threading.Thread):
     #     logging.log(25, "Sending Message: \n{}".format(message))
     #     return message.encode()
 
-    # def my_recv_all(self, s, timeout=2):
-    #     """
-    #     Regardless of message size, ensure that entire message is received
-    #     from server.  Timeout specifies time to wait for additional socket
-    #     stream.
+    def my_recv_all(self, s, timeout=2):
+        """
+        Regardless of message size, ensure that entire message is received
+        from server.  Timeout specifies time to wait for additional socket
+        stream.
 
-    #     param: s <class 'socket.socket'>
-    #             A socket connection to server.
-    #     return: <class 'str'>
-    #             A string containing all info sent.
-    #     """
-    #     # try:
-    #     # make socket non blocking
-    #     s.setblocking(0)
+        param: s <class 'socket.socket'>
+                A socket connection to server.
+        return: <class 'str'>
+                A string containing all info sent.
+        """
+        # try:
+        # make socket non blocking
+        s.setblocking(0)
 
-    #     # total data partwise in an array
-    #     total_data = []
-    #     data = ''
+        # total data partwise in an array
+        total_data = []
+        data = ''
 
-    #     # beginning time
-    #     begin = time.time()
-    #     while 1:
-    #         # if you got some data, then break after timeout
-    #         if total_data and time.time()-begin > timeout:
-    #             break
+        # beginning time
+        begin = time.time()
+        while 1:
+            # if you got some data, then break after timeout
+            if total_data and time.time()-begin > timeout:
+                break
 
-    #         # if you got no data at all, wait a little longer, twice the timeout
-    #         elif time.time()-begin > timeout*2:
-    #             break
+            # if you got no data at all, wait a little longer, twice the timeout
+            elif time.time()-begin > timeout*2:
+                break
 
-    #         # recv something
-    #         try:
-    #             data = s.recv(8192).decode()
-    #             if data:
-    #                 total_data.append(data)
-    #                 # change the beginning time for measurement
-    #                 begin = time.time()
-    #             else:
-    #                 # sleep for sometime to indicate a gap
-    #                 time.sleep(0.1)
-    #         except:
-    #             pass
+            # recv something
+            try:
+                data = s.recv(8192).decode()
+                if data:
+                    total_data.append(data)
+                    # change the beginning time for measurement
+                    begin = time.time()
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
 
-    #     # join all parts to make final string
-    #     return ''.join(total_data)
+        # join all parts to make final string
+        return ''.join(total_data)
 
     # def has_correct_files(self):
     #     ''' Image Check Files'''
